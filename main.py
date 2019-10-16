9ed65015204405fa67e093be77a54ad043943af2
Faizan Amin <faizanfaizanamin@gmail.com>
2019-10-16 22:36:02 +0500

    getPath implementation

--- main.py
+++ main.py
@@ -4,30 +4,31 @@ from utils import process_tree
 from storage import create_folder, delete_folder, upload_blob, list_blobs
 app = Flask(__name__)
 
+# Jsonify data properly in json format
 def jsonifyData(data):
     my_json = data.decode('utf8')
     data = json.loads(my_json)
     return data
 
+# Get complete main path from request
 def getPath(data):
     return os.path.join(data["userId"], data["courseId"], data["assignmentId"])
 
 
 @app.route("/")
 def main():
-    return "Gcloud app is working"
+    return "GCloud app is working"
 
 @app.route("/create-folder", method=["POST"])
 def createFolderRequest():
     data = jsonifyData(request.data)
-    print(getPath(data))
-    resp = create_folder(os.path.join(getPath, data["path"]))
+    resp = create_folder(os.path.join(getPath(data), data["path"]))
     return jsonify({"type":"success"})
 
 @app.route("/delete-folder", method=["POST"])
 def deleteFolderRequest():
     data = jsonifyData(request.data)
-    resp = delete_folder(os.path.join(getPath, data["path"]))
+    resp = delete_folder(os.path.join(getPath(data), data["path"]))
     return jsonify({"type":"success"})
 
 
@@ -36,15 +37,18 @@ def uploadFileRequest():
     data = jsonifyData(request.data)
     with open("temp/" + data["path"], "w+") as file:
         file.write(data["code"])
-    resp = upload_blob("temp/" + data["path"], os.path.join(getPath, data["path"]))
+    resp = upload_blob("temp/" + data["path"], os.path.join(getPath(data), data["path"]))
     os.remove("temp/" + data["path"])
     return jsonify({"type":"success"})
 
 @app.route("/get-dirs-structured-data", method=["POST"])
 def getDirsData():
     data = jsonifyData(request.data)
+    # Main path
     mainPath = getPath(data)
+    # Data list from Google Storage
     data = list_blobs(mainPath)
+    # Complete json object with files and contents
     finalData = process_tree(data, mainPath)
     return jsonify({"type":"success", "data" : finalData})
 
