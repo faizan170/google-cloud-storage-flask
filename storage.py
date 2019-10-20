0b89694ed1258c2d2530be10550e29fcea1f8165
Faizan Amin <faizanfaizanamin@gmail.com>
2019-10-20 16:07:01 +0500

    exception handling added

--- storage.py
+++ storage.py
@@ -7,80 +7,103 @@ from google.cloud import storage
 from google.oauth2 import service_account
 # Instantiates a client
 
-#credentials = service_account.Credentials.from_service_account_file('app-devfever-c5f74595fdd8.json')
+# Added service account
 storage_client = storage.Client.from_service_account_json('app-devfever-c5f74595fdd8.json')
 
 def create_bucket(bucket_name = "checkma"):
     """Creates a new bucket."""
-    bucket = storage_client.create_bucket(bucket_name)
-    print('Bucket {} created'.format(bucket.name))
+    try:
+        bucket = storage_client.create_bucket(bucket_name)
+        print('Bucket {} created'.format(bucket.name))
+        return True
+    except:
+        return False
 
 def upload_blob(source_file_name, destination_blob_name, bucket_name = "checkma"):
     """Uploads a file to the bucket."""
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_filename(source_file_name)
+        blob.upload_from_filename(source_file_name)
 
-    print('File {} uploaded to {}.'.format(
-        source_file_name,
-        destination_blob_name))
+        print('File {} uploaded to {}.'.format(
+            source_file_name,
+            destination_blob_name))
+        return True
+    except:
+        return False
 
 
 def download_blob(source_blob_name, destination_file_name, bucket_name = "checkma"):
     """Downloads a blob from the bucket."""
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(source_blob_name)
 
-    blob.download_to_filename(destination_file_name)
+        blob.download_to_filename(destination_file_name)
 
-    print('Blob {} downloaded to {}.'.format(
-        source_blob_name,
-        destination_file_name))
-
+        print('Blob {} downloaded to {}.'.format(
+            source_blob_name,
+            destination_file_name))
+        return True
+    except:
+        return False
 
 def delete_blob(blob_name, bucket_name = "checkma"):
     """Deletes a blob from the bucket."""
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(blob_name)
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
 
-    blob.delete()
+        blob.delete()
 
-    print('Blob {} deleted.'.format(blob_name))
+        print('Blob {} deleted.'.format(blob_name))
+    except:
+        return False
 
 
 def list_blobs(parent="", bucket_name = "checkma"):
     """Lists all the blobs in the bucket."""
-
-    # Note: Client.list_blobs requires at least package version 1.17.0.
-    blobs = storage_client.list_blobs(bucket_name, prefix=parent)
-    listData = []
-    for blob in blobs:
-        listData.append(blob.name.replace(parent, ""))
-    return listData
+    try:
+        # Note: Client.list_blobs requires at least package version 1.17.0.
+        blobs = storage_client.list_blobs(bucket_name, prefix=parent)
+        listData = []
+        for blob in blobs:
+            listData.append(blob.name.replace(parent, ""))
+        return listData
+    except:
+        return False
 
 def create_folder(path, bucket_name = "checkma"):
     """ Create a new folder """
-    bucket = storage_client.get_bucket(bucket_name)
-    if path[-1] != "/":
-        path += "/"
-    blob = bucket.blob(path)
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        if path[-1] != "/":
+            path += "/"
+        blob = bucket.blob(path)
 
-    blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+        blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+        return True
+    except:
+        return False
 
 
 def delete_folder(path, bucket_name = "checkma"):
     """ Delete a folder """
-    if path[-1] != "/":
-        path += "/"
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(path)
+    try:
+        if path[-1] != "/":
+            path += "/"
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(path)
 
-    blob = bucket.blob(path)
+        blob = bucket.blob(path)
 
-    blob.delete()
+        blob.delete()
 
-    print('Blob {} deleted.'.format(path))
+        print('Blob {} deleted.'.format(path))
+    except:
+        return False
 
 def check_folder_exists(folderPath, bucket_name="checkma"):
     """ check if path/file exists in bucket or not """
